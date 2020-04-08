857faf93487bb64ec96530fb824f3051f9375b12
zhaoch <zhaoch@knownsec.com>
2020-04-08 23:27:20 +0800

    load & dump & use double

--- frk_dict.h
+++ frk_dict.h
@@ -7,17 +7,20 @@
 
 typedef void* (*frk_malloc_fp)(int64_t size, void* data);
 typedef void (*frk_free_fp)(void* ptr, void *data);
+typedef char* (*frk_to_string_fp)(void *data, char* buf, int64_t len);
+
 typedef struct frk_dict_s frk_dict_t;
 typedef struct frk_str_s frk_str_t;
-typedef struct frk_iter_s frk_iter_t;
+typedef struct frk_dict_iter_s frk_dict_iter_t;
 typedef struct frk_node_s frk_node_t;
-
+typedef struct frk_obj_s frk_obj_t;
 
 typedef enum {
     FRK_NIL = 0,
-    FRK_INT = 1,
-    FRK_STR = 2,
-    FRK_DICT = 3
+    FRK_NUM,
+    FRK_STR,
+    FRK_DICT,
+    FRK_OBJ,
 } frk_type_e;
 
 
@@ -28,13 +31,23 @@ struct frk_str_s
 };
 
 
+struct frk_obj_s
+{
+    frk_to_string_fp string;
+    frk_malloc_fp malloc;
+    frk_free_fp free;
+    void* malloc_data;
+    void* data;
+};
+
+
 struct frk_dict_s
 {
     int64_t count;
     int64_t bucket_count;
     frk_malloc_fp malloc;
     frk_free_fp free;
-    void* data;
+    frk_obj_t* data;
     frk_node_t** buckets;
 };
 
@@ -48,14 +61,15 @@ struct frk_node_s
     frk_type_e type;
     union
     {
-        int64_t i;
+        double n;
         frk_str_t* s;
         frk_dict_t* d;
+        void* o;
     };
 };
 
 
-struct frk_iter_s
+struct frk_dict_iter_s
 {
     int64_t bucket;
     frk_node_t* node;
@@ -64,19 +78,23 @@ struct frk_iter_s
 
 frk_dict_t* frk_new_dict(frk_malloc_fp m, frk_free_fp f, void* data);
 
-int64_t* frk_insert_int(frk_dict_t *d, char *key, int64_t key_len, int64_t val);
+double* frk_insert_num(frk_dict_t *d, char *key, int64_t key_len, double val);
 frk_str_t* frk_insert_string(frk_dict_t *d, char *key, int64_t key_len, char *val, int64_t val_len);
 frk_dict_t* frk_insert_dict(frk_dict_t *d, char *key, int64_t key_len);
 
 frk_node_t* frk_get_node(frk_dict_t *d, char *key, int64_t key_len);
 frk_dict_t* frk_get_dict(frk_dict_t *d, char *key, int64_t key_len);
-int64_t* frk_get_int(frk_dict_t *d, char *key, int64_t key_len);
+double* frk_get_num(frk_dict_t *d, char *key, int64_t key_len);
 frk_str_t* frk_get_str(frk_dict_t *d, char *key, int64_t key_len);
 
 void frk_remove(frk_dict_t *d, char *key, int64_t key_len);
 void frk_remove_node(frk_dict_t *d, frk_node_t* node);
 
-frk_iter_t* frk_iterator(frk_dict_t *d, frk_iter_t *last, frk_iter_t *next);
+void frk_dict_clear(frk_dict_t *d);
+
+frk_dict_iter_t* frk_dict_iter(frk_dict_t *d, frk_dict_iter_t *last, frk_dict_iter_t *next);
+
 int64_t frk_dict_dump(frk_dict_t *d, char *buffer, int64_t len);
+frk_dict_t* frk_dict_load(frk_dict_t* d, char* json, char **end);
 
 #endif
--- test.c
+++ test.c
@@ -62,24 +62,24 @@ dict_test()
 
     frk_dict_t* root = frk_new_dict(slab_malloc, slab_free, s);
 
-    frk_insert_int(root, "aaa", 3, 1);
-    frk_insert_int(root, "aaa", 3, 2);
+    frk_insert_num(root, "aaa", 3, 1);
+    frk_insert_num(root, "aaa", 3, 2);
     frk_insert_string(root, "bbb", 3, "xyz", 4);
     frk_insert_string(root, "bbb", 3, "xyzzzz", 4);
     frk_dict_t* sub = frk_insert_dict(root, "sub", 3);
-    frk_insert_int(sub, "aaa", 3, 11);
-    frk_insert_int(sub, "aaa", 3, 22);
+    frk_insert_num(sub, "aaa", 3, 11);
+    frk_insert_num(sub, "aaa", 3, 22);
     frk_insert_string(sub, "bbb", 3, "xyz", 4);
     frk_dict_t* subsub = frk_insert_dict(sub, "subsub", 6);
-    frk_insert_int(subsub, "aaa", 3, 111);
-    frk_insert_int(subsub, "aaa", 3, 222);
+    frk_insert_num(subsub, "aaa", 3, 111);
+    frk_insert_num(subsub, "aaa", 3, 222);
     frk_insert_string(subsub, "bbb", 3, "xyz", 4);
 
-    printf("%ld\n", *frk_get_int(root, "aaa", 3));
+    printf("%lf\n", *frk_get_num(root, "aaa", 3));
     printf("%s\n", frk_get_str(root, "bbb", 3)->data);
-    printf("%ld\n", *frk_get_int(sub, "aaa", 3));
+    printf("%lf\n", *frk_get_num(sub, "aaa", 3));
     printf("%s\n", frk_get_str(sub, "bbb", 3)->data);
-    printf("%ld\n", *frk_get_int(subsub, "aaa", 3));
+    printf("%lf\n", *frk_get_num(subsub, "aaa", 3));
     printf("%s\n", frk_get_str(subsub, "bbb", 3)->data);
 
     frk_remove(root, "aaa", 3);
@@ -90,14 +90,14 @@ dict_test()
     char key[1];
     for (; i < 200; i++) {
         key[0] = (char)i;
-        frk_insert_int(root, key, 1, i);
+        frk_insert_num(root, key, 1, i);
     }
-    printf("%p\n", frk_get_int(root, "0", 1));
+    printf("%p\n", frk_get_num(root, "0", 1));
     for (i = 0; i < 200; i++) {
         key[0] = (char)i;
         frk_remove(root, key, 1);
     }
-    printf("%p\n", frk_get_int(root, "0", 1));
+    printf("%p\n", frk_get_num(root, "0", 1));
 }
 
 
@@ -111,53 +111,93 @@ void app_test()
     frk_dict_t* a = frk_insert_dict(sites, "a.com", strlen("a.com"));
 
     frk_dict_t* wild = frk_insert_dict(a, ".a.com", strlen(".a.com"));
-    frk_insert_int(wild, "timeout", strlen("timeout"), 10);
-    frk_insert_int(wild, "buffer", strlen("buffer"), 4096);
+    frk_insert_num(wild, "timeout", strlen("timeout"), 10);
+    frk_insert_num(wild, "buffer", strlen("buffer"), 4096);
     frk_insert_string(wild, "upstream", strlen("upstream"), "127.0.0.1:5000", strlen("127.0.0.1:5000"));
 
     frk_dict_t* www = frk_insert_dict(a, "www.a.com", strlen("www.a.com"));
-    frk_insert_int(www, "timeout", strlen("timeout"), 5);
-    frk_insert_int(www, "buffer", strlen("buffer"), 1024);
+    frk_insert_num(www, "timeout", strlen("timeout"), 5);
+    frk_insert_num(www, "buffer", strlen("buffer"), 1024);
     frk_insert_string(www, "upstream", strlen("upstream"), "192.168.0.3:5000", strlen("192.168.0.3:5000"));
 
-    frk_dict_t* news = frk_insert_dict(a, "news.a.com", strlen("www.a.com"));
-    frk_insert_int(news, "timeout", strlen("timeout"), 5);
+    frk_dict_t* news = frk_insert_dict(a, "news.a.com", strlen("news.a.com"));
+    frk_insert_num(news, "timeout", strlen("timeout"), 5);
     frk_insert_string(news, "upstream", strlen("upstream"), "192.168.0.100:5000", strlen("192.168.0.100:5000"));
     frk_dict_t* black = frk_insert_dict(news, "black_ip", strlen("black_ip"));
-    frk_insert_int(black, "222.222.222.1", strlen("222.222.222.1"), 1);
-    frk_insert_int(black, "222.222.222.2", strlen("222.222.222.2"), 1);
-    frk_insert_int(black, "222.222.222.3", strlen("222.222.222.3"), 1);
-    frk_insert_int(black, "222.222.222.4", strlen("222.222.222.4"), 1);
+    frk_insert_num(black, "222.222.222.1", strlen("222.222.222.1"), 1);
+    frk_insert_num(black, "222.222.222.2", strlen("222.222.222.2"), 1);
+    frk_insert_num(black, "222.222.222.3", strlen("222.222.222.3"), 1);
+    frk_insert_num(black, "222.222.222.4", strlen("222.222.222.4"), 1);
 
     frk_dict_t* b = frk_insert_dict(sites, "b.com", strlen("b.com"));
     frk_dict_t* wild_b = frk_insert_dict(b, ".b.com", strlen(".bcom"));
-    frk_insert_int(wild_b, "timeout", strlen("timeout"), 100);
-    frk_insert_int(wild_b, "buffer", strlen("buffer"), 40960);
+    frk_insert_num(wild_b, "timeout", strlen("timeout"), 100);
+    frk_insert_num(wild_b, "buffer", strlen("buffer"), 40960);
 
     frk_dict_t* c = frk_insert_dict(sites, "c.com", strlen("c.com"));
     frk_dict_t* www_c = frk_insert_dict(c, "www.c.com", strlen("www.c.com"));
-    frk_insert_int(www_c, "timeout", strlen("timeout"), 200);
-    frk_insert_int(www_c, "buffer", strlen("buffer"), 10240);
+    frk_insert_num(www_c, "timeout", strlen("timeout"), 200);
+    frk_insert_num(www_c, "buffer", strlen("buffer"), 10240);
 
     char *buffer = calloc(1024, 100);
 
     // for each 
-    frk_iter_t tmp, *i = frk_iterator(black, NULL, &tmp);
-    for (; i != NULL; i = frk_iterator(black, i, &tmp)) {
+    frk_dict_iter_t tmp, *i = frk_dict_iter(black, NULL, &tmp);
+    for (; i != NULL; i = frk_dict_iter(black, i, &tmp)) {
         // i->node->key->data may not ends with '\0' don't do that in product
-        printf("%s: %ld\n", i->node->key->data, i->node->i);
+        printf("%s: %lf\n", i->node->key->data, i->node->n);
     }
 
     frk_dict_dump(sites, buffer, 102400);
-    printf("%s\n", buffer);
-
-    // todo foreach
+    printf("sites:\n %s\n", buffer);
 }
 
-int main() {
+
+void 
+load_test()
+{
+    char *json = 
+        "{\"a.com\":"
+            "{\"news.a.com\":{\"timeout\":5.000000,\"upstream\":\"192.168.0.100:5000\","
+                "\"black_ip\":{\"222.222.222.4\":1.000000,\"222.222.222.1\":1.000000,\"222.222.222.2\":1.000000,\"222.222.222.3\":1.000000}},"
+            "\"www.a.com\":{\"buffer\":1024.000000,\"timeout\":5.000000,\"upstream\":\"192.168.0.3:5000\"},"
+            "\".a.com\":{\"buffer\":4096.000000,\"timeout\":10.000000,\"upstream\":\"127.0.0.1:5000\"}},"
+        "\"b.com\":"
+            "{\".b.co\":{\"buffer\":40960.000000,\"timeout\":100.000000}},"
+        "\"c.com\":"
+            "{\"www.c.com\":{\"buffer\":10240.000000,\"timeout\":200.000000}}}";
+
+    void* pool = malloc(size);
+    frk_slab_t *s = frk_new_slab(pool, size);
+    frk_dict_t* sites = frk_new_dict(slab_malloc, slab_free, s);
+    frk_dict_load(sites, json, &json);
+
+    frk_dict_t *a_com = frk_get_dict(sites, "a.com", strlen("a.com"));
+    frk_dict_t *news_a_com = frk_get_dict(a_com, "news.a.com", strlen("news.a.com"));
+    frk_dict_t *black = frk_get_dict(news_a_com, "black_ip", strlen("black_ip"));
+
+    frk_dict_iter_t tmp, *i = frk_dict_iter(black, NULL, &tmp);
+    for (; i != NULL; i = frk_dict_iter(black, i, &tmp)) {
+        // i->node->key->data may not ends with '\0' don't do that in product
+        printf("%s: %lf\n", i->node->key->data, i->node->n);
+    }
+
+    char *buffer = calloc(1024, 100);
+    frk_dict_dump(sites, buffer, 102400);
+    printf("load:\n %s\n", buffer);
+
+    frk_dict_clear(sites);
+    memset(buffer, 0, 102400);
+    frk_dict_dump(sites, buffer, 102400);
+    printf("load:\n %s\n", buffer);
+}
+
+int
+main() {
     slab_test();
     dict_test();
     app_test();
+    load_test();
 
     return 0;
 }
